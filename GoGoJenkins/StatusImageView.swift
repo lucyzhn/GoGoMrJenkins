61ee0484f2ced739571b5819337c0b99e76ca857
Lucy Zhan <lucy.zhan@classpass.com>
2021-10-13 14:41:33 -0400

    add build status images

--- GoGoJenkins/StatusImageView.swift
+++ GoGoJenkins/StatusImageView.swift
@@ -12,7 +12,14 @@ class StatusImageView: NSImageView {
     override func draw(_ dirtyRect: NSRect) {
         super.draw(dirtyRect)
         
-        self.image = NSImage(named: "Pickaxe@2x.png")
+        // PENDING
+//        self.image = NSImage(named: "Pickaxe@2x.png)
+        
+        // SUCCESS
+//        self.image = NSImage(named: "Checkmark@2x.png")
+        
+        // FAIL
+//        self.image = NSImage(named: "FailedJenkins@2x.png")
     }
     
 }
